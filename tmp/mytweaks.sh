a716ca548eac747d525d9b4c2e7c39b9cd7908a6
Ben Edmunds <Tigger2014@users.noreply.github.com>
2018-08-06 19:47:26 +0100

    Update mytweaks.sh

--- tmp/mytweaks.sh
+++ tmp/mytweaks.sh
@@ -5,21 +5,6 @@ bp="/system/build.prop"
 busybox mount /system
 busybox mount /data
 
-
-
-if [ -f /system/build.prop.bak ]; 
-  then
-    rm -rf $bp
-    cp $bp.bak $bp
-  else
-    cp $bp $bp.bak
-fi
-
-
-echo " " >> $bp
-echo "# unCoRrUpTeD build.prop editor" >> $bp
-echo " " >> $bp
-
 for mod in update-build.prop;
   do
 
